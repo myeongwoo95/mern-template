ddb83805b4005fe36284dcd28f16d6e95cdd161c
walle <myeongwoo95@naver.com>
2023-07-07 05:03:06 +0900

    ":>"

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+// models
+const { User } = require("./models/User");
+
 // express.js
 const express = require("express");
 const app = express();
 const port = 5000;
+
+// body-parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true })); // x-www-form-urlencoded를 받을 수 있게 설정
+app.use(bodyParser.json()); // json를 받을 수 있게 설정
 
 // mongoDB
 const mongoose = require("mongoose");
@@ -10,6 +18,27 @@ mongoose
   .then(() => console.log("MongoDB에 연결되었습니다."))
   .catch((error) => console.log("MongoDB 연결에 실패했습니다.", error));
 
-app.get("/", (req, res) => res.send("Hello world!"));
+// controller
+app.listen(port, () => console.log(`Express app listening on port ${port}!`));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/", (req, res) => res.send("Hello world!!!!!"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then((userInfo) => {
+      res.json({
+        success: true,
+        userInfo,
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json({
+        success: false,
+        err,
+      });
+    });
+});
